2714a8ba6d6b916e5bd373ff680df9569a4c9eef
李铭昕 <715557344@qq.com>
2024-04-17 21:34:51 +0800

    Update Http.php

--- src/Http/Http.php
+++ src/Http/Http.php
@@ -18,7 +18,7 @@ interface Http
 {
     public const DEFAULT_PROTOCOL_VERSION = '1.1';
 
-    public static function packRequest(string $method, string|Stringable $uri, array $headers = [], string|Stringable $body = '', string $protocolVersion = self::DEFAULT_PROTOCOL_VERSION): string;
+    public static function packRequest(string $method, string|Stringable $path, array $headers = [], string|Stringable $body = '', string $protocolVersion = self::DEFAULT_PROTOCOL_VERSION): string;
 
     public static function packResponse(int $statusCode, string $reasonPhrase = '', array $headers = [], string|Stringable $body = '', string $protocolVersion = self::DEFAULT_PROTOCOL_VERSION): string;
 }
